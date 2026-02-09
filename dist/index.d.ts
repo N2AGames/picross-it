18aaaf38fc67ee1395a59156bdfe7bd9f6a77de0
Not2Awesome Games <not2awesomegames@gmail.com>
2026-02-09 09:55:44 +0100

    Elimino el dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-export { processImageData, processCanvasImage, processImageUrl, processImageFile, } from './imageProcessor';
-export * from './utils/pixelAnalysis';
-export * from './utils/imageAnalysis';
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-// Main entry point
-export * from './types';
-export { processImageData, processCanvasImage, processImageUrl, processImageFile, } from './imageProcessor';
-export * from './utils/pixelAnalysis';
-export * from './utils/imageAnalysis';
-//# sourceMappingURL=index.js.map
